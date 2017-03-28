6195d5be4455d5ffa2df1999a03c23d817ad2d60
Will Robot <robot.will.me@gmail.com>
2017-03-28 13:20:08 +0800

    Fixed an error.

--- libs/stino_runtime/selected.py
+++ libs/stino_runtime/selected.py
@@ -84,7 +84,7 @@ def get_platform_name_by_arch(arduino_info, pkg_name, ptfm_arch):
 
 def get_platform_arch_by_name(arduino_info, pkg_name, ptfm_name):
     """."""
-    ptfm_arch = ptfm_name
+    ptfm_arch = ''
     pkgs_info = arduino_info.get('packages', {})
     ptfm_names = get_platform_names(pkgs_info, pkg_name)
     ptfm_arches = get_platform_arches(pkgs_info, pkg_name)
@@ -107,9 +107,10 @@ def get_full_platform_info(arduino_info, pkg, ptfm, ver):
 
     cur_ptfm = get_platform_name_by_arch(arduino_info, cur_pkg,
                                          cur_arch)
-    if not cur_ver and cur_ptfm:
+    if not cur_ver:
         vers = get_platform_versions(pkgs_info, cur_pkg, cur_ptfm)
-        cur_ver = vers[-1]
+        if vers:
+            cur_ver = vers[-1]
 
     ptfm_info = get_platform_info(pkgs_info, cur_pkg, cur_ptfm, cur_ver)
 
